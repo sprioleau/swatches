e024d6b955c7d8644cd8642eafe0a6a692ad5b7e
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-12-21 23:14:34 -0500

    Update description

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ class MyDocument extends Document {
 					<meta charSet="UTF-8" />
 					<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 					<link rel="icon" href="/favicon.png" />
-					<meta name="description" content="The most beautiful way to lookup all of the CSS colors you love." />
+					<meta name="description" content="The most beautiful way to see HTML colors in context." />
 				</Head>
 				<body>
 					<Main />
