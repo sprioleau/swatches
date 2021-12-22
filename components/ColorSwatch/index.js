1bc2e90895892cb9044c12b5714ad98b657034ca
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-12-21 20:35:21 -0500

    Use next/link component for changing routes

--- components/ColorSwatch/index.js
+++ components/ColorSwatch/index.js
@@ -1,33 +1,33 @@
 import React from "react";
-import { useRouter } from "next/router";
-import { selectSetSelectedColor } from "../../store/selectors";
-import useStore from "../../store";
+import Link from "next/link";
 
 const ColorSwatch = ({ color }) => {
 	const { name, prettyName, hex, rgbString } = color;
-	const router = useRouter();
-	const setSelectedColor = useStore(selectSetSelectedColor);
-
-	const handleClick = (e) => {
-		setSelectedColor(color);
-		router.push(`colors/${name}`);
-	};
 
 	return (
-		<div className="color-swatch" onClick={handleClick} role="button" tabIndex={0} aria-label="color button">
-			<div
-				className="color-swatch__swatch"
-				data-name={name}
-				data-hex={hex}
-				data-rgb={rgbString}
-				style={{ backgroundColor: hex }}
-			/>
-			<div className="color-swatch__details">
-				<h3 className="color-swatch__label">SWATCH</h3>
-				<h3 className="color-swatch__hex">{hex.substring(1).toUpperCase()}</h3>
-				<h3 className="color-swatch__name">{prettyName}</h3>
-			</div>
-		</div>
+		<Link
+			href={`colors/${name}`}
+			className="color-swatch"
+			role="button"
+			tabIndex={0}
+			aria-label="color button"
+			passHref
+		>
+			<a className="color-swatch__link link-wrapper">
+				<div
+					className="color-swatch__swatch"
+					data-name={name}
+					data-hex={hex}
+					data-rgb={rgbString}
+					style={{ backgroundColor: hex }}
+				/>
+				<div className="color-swatch__details">
+					<h3 className="color-swatch__label">SWATCH</h3>
+					<h3 className="color-swatch__hex">{hex.substring(1).toUpperCase()}</h3>
+					<h3 className="color-swatch__name">{prettyName}</h3>
+				</div>
+			</a>
+		</Link>
 	);
 };
 
